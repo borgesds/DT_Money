dc3b90da497f55237e619b362b08fa6535b0f1cb
André Borges <borges.an@hotmail.com>
2022-12-26 11:17:32 -0300

    up Code

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,5 +1,6 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
 import { PriceHighlight, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transaction() {
@@ -10,6 +11,8 @@ export function Transaction() {
             <Summary />
 
             <TransactionsContainer>
+                <SearchForm />
+                
                 <TransactionsTable>
                     <tbody>
                         <tr>
--- src/pages/Transactions/styles.ts
+++ src/pages/Transactions/styles.ts
@@ -11,7 +11,7 @@ export const TransactionsTable = styled.table`
   width: 100%;
   border-collapse: separate; //cria um separador
   border-spacing: 0 0.5rem; //espessura do separador
-  /* margin-top: 1.5rem; */
+  margin-top: 1.5rem;
 
   td {
     padding: 1.25rem 2rem;
